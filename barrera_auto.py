dfd18594f32e67a30442eac94c913b51ddc00648
Jordi_Torrents <jordi.torrentsm@gmail.com>
2020-03-07 13:56:18 +0100

    millora del mètoda per automatitzar les barreres, ara funciona guaión

--- barrera_auto.py
+++ barrera_auto.py
@@ -14,7 +14,6 @@ def R():
 def T():
     return np.sum(prob[int(n/2):])*dx
 
-# high = float(input('Alçada de la barrera de potencial ( de l.ordre de 15.0)? '))
 # BC = int(input('Condicions de contorn periòdiques (0), potencial infinit (1) o obertes (2) ? '))
 BC=1
 a = 5
@@ -25,27 +24,26 @@ w = 2
 m  = 1
 L=5
 x0 =-L/2
-E = k**2/(2*m)
 V = np.zeros(n)
-ample=ample=int(0.5*(n/2)/20)
+ample=int(0.5*(n/2)/20)
 x=np.linspace(-L,L,n)
 dx=(2*L)/(n-1)
-dt=0.5/(2/(m*dx**2)+np.max(V))
+dt=0.9/(2/(m*dx**2)+np.max(V))
 
 R_arr=np.empty(50)
 T_arr=np.empty(50)
-alcada = np.linspace(0,1.5,50)*E
+alcada = np.linspace(0,1.5,50)
 for i in range(50):
     psi=psi_0(x) ; psi_old = psi_0(x)
-    V[int(n/2)-ample:int(n/2)+ample]=alcada[i]
-    nf.euler_b(psi=psi, psi_old=psi_old, steps=13000, dt=dt, m=m, v=V, dx=dx, bc=BC)
+    V[int(n/2)-ample:int(n/2)+ample]=alcada[i]*k**2/(2*m)
+    nf.euler_b(psi=psi, psi_old=psi_old, steps=int(0.35/dt), dt=dt, m=m, v=V, dx=dx, bc=BC)
     prob = np.abs(psi)**2
     R_arr[i]=R()
     T_arr[i]=T()
 
 plt.figure()
-plt.plot(alcada/E,R_arr, 'C0.-', label='R')
-plt.plot(alcada/E,T_arr, 'C1.-', label='T')
+plt.plot(alcada,R_arr, 'C0.-', label='R')
+plt.plot(alcada,T_arr, 'C1.-', label='T')
 plt.legend()
 plt.show()
 
